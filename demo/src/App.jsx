2f93f6927e180cd1a74b4a7b1dd934c07c3f98e1
Milan Hauth <milahu@gmail.com>
2022-09-13 18:15:24 +0200

    init demo

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,9 +1,169 @@
-import logo from './logo.svg';
+import { createSignal, onMount, For } from "solid-js";
+
+//import logo from './logo.svg';
 import styles from './App.module.css';
 
+import { NixEval, NixEvalError } from "../../src/nix-eval.js";
+
+//import { Xterm } from './xterm.jsx'
+import { Xterm } from './solidjs-xterm-component/xterm.jsx'
+import LocalEchoController from 'local-echo';
+import { WebLinksAddon } from 'xterm-addon-web-links';
+
+// wrapper is not working with vite bundler
+// import { configure as getStringify } from '../../src/safe-stable-stringify/esm/wrapper.js'
+// Uncaught SyntaxError: The requested module safe-stable-stringify/index.js does not provide an export named 'default'
+import { configure as getStringify } from '../../src/safe-stable-stringify/index.esm.js'
+// https://github.com/BridgeAR/safe-stable-stringify/issues/32
+// https://github.com/BridgeAR/safe-stable-stringify/issues/19
+
+
+const stringify = getStringify({
+  maximumDepth: 2,
+  maximumBreadth: 10,
+  indent: "  ",
+})
+
+// FIXME not working?
+import { FitAddon } from 'xterm-addon-fit';
+
+import * as nixReplHelp from './nix-repl-help.js'
+
 function App() {
+
+  let nixEval;
+  let terminal;
+
+  onMount(async () => {
+    nixEval = new NixEval();
+  })
+
+  function onTerminal(_terminal) {
+    terminal = _terminal;
+
+    // colors
+    // TODO ubuntu theme https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+    /*
+    terminal.options.theme = {
+      background: "black",
+      foreground: "white",
+    };
+    */
+
+    // FIXME div height
+    // fit terminal to container element
+    // fixed by setting Xterm.style?
+    const fitAddon = new FitAddon();
+    terminal.loadAddon(fitAddon);
+
+    // clickable links
+    /*
+    function onLinkClick(_event, url) {
+      console.log(`clicked link: ${url}`);
+    }
+    */
+    //terminal.loadAddon(new WebLinksAddon(onLinkClick));
+    // default handler: open the link
+    terminal.loadAddon(new WebLinksAddon());
+
+    // start writing to terminal
+
+    // greeting
+    const nixEvalVersion = '0.0.1';
+    terminal.writeln(`Welcome to nix-eval.js ${nixEvalVersion}. Type :? for help. ArrowUp for history.`);
+    terminal.writeln('');
+
+    // simple link
+    //terminal.writeln('click here: https://xtermjs.org/');
+
+    // OSC 8 hyperlink escape codes
+    // not working
+    //terminal.writeln('click here: \x1b]8;;https://xtermjs.org\x07OSC 8 hyperlink text\x1b]8;;\x07');
+
+    function evalLine(line) {
+      // https://ansi.gabebanks.net/ # ANSI escape code generator
+      const start = '\x1B[';
+      const bold = '1';
+      const red = '31';
+      const redbold = `${start}${red};${bold}m`;
+      const reset = '\x1B[0m';
+      //const end = 'm';
+      //const sep = ';';
+      //const magenta = '35';
+      //const magentabold = `${start}${magenta};${bold}m`;
+
+      // eval javascript
+      //return JSON.stringify(eval(line), null, 2);
+
+      // eval nix repl
+      if (line == ':?') {
+        return nixReplHelp.main.join('\r\n');
+      }
+      if (line == ':? e') {
+        return nixReplHelp.expressions.join('\r\n');
+      }
+      if (line == ':? o') {
+        return nixReplHelp.operations.join('\r\n');
+      }
+
+      // eval nix
+      try {
+        const result = nixEval.eval(line);
+        console.log(`eval result:`, result);
+        if (result === undefined) return '';
+        //return String(result).replace(/\n/g, '\r\n'); // TODO implement custom toString
+        return stringify(result).replace(/\n/g, '\r\n');
+      }
+      catch (error) {
+        if (error instanceof NixEvalError) {
+          return `${redbold}error:${reset} ${error.message}`;
+        }
+        else {
+          console.log(error);
+          return `${redbold}FIXME${reset} ${error.name}: ${error.message}`;
+        }
+      }
+    }
+
+
+
+    // read lines
+
+    const localEcho = new LocalEchoController(null, {
+      historySize: 9999, // workaround for https://github.com/wavesoft/local-echo/issues/34
+    });
+    localEcho.history.entries = nixReplHelp.demos.slice();
+    terminal.loadAddon(localEcho);
+    // note: the main local-echo repo is not maintained
+    // TODO pre-fill command history with demo commands
+    // TODO autocompletion
+    // TODO fix history bug https://github.com/wavesoft/local-echo/issues/34
+    // https://github.com/wavesoft/local-echo/pull/32
+
+    const promptString = 'nix-repl> ';
+
+    function readLine() {
+
+      // Read a single line from the user
+      localEcho.read(promptString)
+        .then(line => {
+          console.log(`user input : ${line}`);
+          const result = evalLine(line);
+          if (result !== undefined) {
+            //console.log(`Result: ${result}`);
+            terminal.write(result.toString() + '\r\n\r\n');
+          }
+          readLine(); // read next line
+        })
+        .catch(error => console.log(`Error reading line: ${error}`))
+      ;
+    }
+    readLine(); // start loop
+  }
+
   return (
     <div class={styles.App}>
+      {/*
       <header class={styles.header}>
         <img src={logo} class={styles.logo} alt="logo" />
         <p>
@@ -18,6 +178,21 @@ function App() {
           Learn Solid
         </a>
       </header>
+      */}
+      {/*
+      <main class={styles.main} contenteditable>
+        nix-repl&gt;&nbsp;
+      </main>
+      */}
+      <main class={styles.main}>
+        <Xterm
+          onTerminal={onTerminal}
+          style={{
+            'flex-grow': 1, // full height
+            'display': 'flex',
+          }}
+        />
+      </main>
     </div>
   );
 }
---
+++ demo/src/nix-repl-help.js
@@ -0,0 +1,57 @@
+export const main = [
+  'The following commands are available:',
+  '',
+  '  <expr>        Evaluate and print expression',
+  '  :? e          Show available expressions',
+  '  :? o          Show available operations',
+];
+
+export const expressions = [
+  'The following expressions are available:',
+  '',
+  '  null          Null',
+  '  true          True',
+  '  false         False',
+  '',
+  '  1             Integer',
+  '  1.2           Float',
+  '',
+  '  1+2           Add',
+  '  1-2           Sub',
+  '  1*2           Mul',
+  '  1/2           Div',
+  '',
+  '  [1 2]         List',
+  '  {a=1;b=2;}    AttrSet',
+  '  {a=1;}.a      Select',
+];
+
+export const operations = [
+  'The following operations are available:',
+  '',
+  '  __add 1 2',
+  '  __sub 1 2',
+  '  __mul 1 2',
+  '  __div 1 2',
+  '',
+  '  __head [1 2]',
+  '  __tail [1 2]',
+  '  __elemAt [1 2] 0',
+];
+
+export const demos = [
+  '1+2',
+  '1-2',
+  '1*2',
+  '1/2',
+  '[1 2]',
+  '{a=1;b=2;}',
+  '{a=1;}.a',
+  '__add 1 2',
+  '__sub 1 2',
+  '__mul 1 2',
+  '__div 1 2',
+  '__head [1 2]',
+  '__tail [1 2]',
+  '__elemAt [1 2] 0',
+];
