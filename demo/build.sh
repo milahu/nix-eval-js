c7d0849ec2e8a0da842633fb9c5bc88fe52836ca
Milan Hauth <milahu@gmail.com>
2022-09-25 00:56:08 +0200

    add demo/build.sh

---
+++ demo/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "removing nested node_modules, so vite does not use them. fix codemirror error:"
+echo "  Error: Unrecognized extension value in extension set ([object Object])."
+echo "  This sometimes happens because multiple instances of @codemirror/state are loaded, breaking instanceof checks."
+( set -x
+  rm -rf src/codemirror-lang-nix/node_modules
+)
+
+( set -x
+  npm run build
+)
