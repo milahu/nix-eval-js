d4d0d0c9ffb92311f4d6e996b0b5a3109314764a
Milan Hauth <milahu@gmail.com>
2022-09-25 14:01:00 +0200

    fix src/nix-eval.js

--- src/nix-eval.js
+++ src/nix-eval.js
@@ -7,15 +7,13 @@ import { parser as LezerParserNix } from "./lezer-parser-nix/dist/index.js"
 //import { parser as LezerParserNix } from "../demo/src/codemirror-lang-nix/src/lezer-parser-nix/dist/index.js"
 //import { parser as LezerParserNix } from "../demo/src/codemirror-lang-nix/dist/index.js"
 
-
-
-import { setThunkOfNodeType } from "./nix-thunks.js"
+import { thunkOfNodeType } from './nix-thunks-lezer-parser.js';
 import { NixEvalError, NixSyntaxError, NixEvalNotImplemented } from "./nix-errors.js"
 
 // "export { ... } from '...'" is not working in vite
 export {
   LezerParserNix,
-  setThunkOfNodeType,
+  thunkOfNodeType,
   NixEvalError,
   NixSyntaxError,
   NixEvalNotImplemented,
@@ -55,6 +53,13 @@ export class NixEval {
     const strictParser = LezerParserNix.configure({
       strict: true, // throw on parse error
     });
+
+    // add thunks to types
+    // TODO? move to evalTree
+    for (const nodeType of strictParser.nodeSet.types) {
+      nodeType.thunk = thunkOfNodeType[nodeType.name];
+    }
+
     let tree;
     try {
       tree = strictParser.parse(source);
@@ -72,171 +77,6 @@ export class NixEval {
   }
 
   evalTree(tree, source) {
-
-    //console.log(`NixEval.evalTree`);
-/*
-    // TODO check tree for parse errors
-NixEvalNotImplemented
-setThunk is empty for token ⚠
-
-*/
-
-    let depth = 0;
-    const cursor = tree.cursor();
-
-
-
-    const rootNode = {};
-
-    Object.defineProperties(rootNode, {
-      'type': { value: "Nix", enumerable: true },
-      'text': { value: source, enumerable: true },
-      'thunk': { value: null, writable: true }, // hidden
-      'depth': { value: depth }, // hidden
-      'parent': { value: null }, // hidden
-      'children': { value: [], enumerable: true },
-    });
-
-    rootNode.toString = nodeToString;
-
-    let parentNode = rootNode;
-
-
-
-    // walk tree of nodes
-
-    // https://en.wikipedia.org/wiki/Tree_traversal#In-order,_LNR
-    // In-order, LNR
-    // 1. Recursively traverse the current node's left subtree. // cursor.firstChild
-    // 2. Visit the current node.
-    // 3. Recursively traverse the current node's right subtree. // cursor.nextSibling
-    // https://rosettacode.org/wiki/Tree_traversal#JavaScript
-    // https://github.com/lezer-parser/common/blob/main/src/tree.ts # TreeCursor.iterate
-
-    while (true) {
-
-      const cursorText = source.slice(cursor.from, cursor.to);
-      const cursorType = cursor.name;
-      //const cursorTypeId = cursor.type.id; // TODO use numeric types
-
-      // defineProperties:
-      // hide some properties -> prettier output from console.dir
-      // make some properties read-only -> better performance?
-
-      const thisNode = {};
-
-      Object.defineProperties(thisNode, {
-        'type': { value: cursorType, enumerable: true },
-        'text': { value: cursorText, enumerable: true },
-        'thunk': { value: null, writable: true }, // hidden
-        'depth': { value: depth }, // hidden
-        'parent': { value: parentNode }, // hidden
-        'children': { value: [], enumerable: true },
-      });
-
-      thisNode.toString = nodeToString;
-
-      parentNode.children.push(thisNode);
-
-
-
-      // 1. Recursively traverse the current node's left subtree.
-      // case "left": if (this.left) this.left.walk(func, order); break;
-      //console.log(`${'  '.repeat(depth)}1. ${cursorType}: ${cursorText}`);
-      //console.log(`step 1: thisNode:`); console.dir(thisNode);
-
-      // deep first
-      // try to move down
-      if (cursor.firstChild()) {
-        // moved down
-        parentNode = thisNode;
-        depth++;
-        continue;
-      }
-
-
-
-      // 2. Visit the current node.
-      // case "this": func(this.value); break;
-
-      //console.log(`step 2: thisNode:`); console.dir(thisNode);
-      //console.log(`${'  '.repeat(depth)}2. ${cursorType}: ${cursorText}`);
-
-      // bottom-up
-      //console.log(`NixEval.evalTree: ${'  '.repeat(depth)}${cursorTypeId}=${cursorType}: ${cursorText} rootNode`, rootNode, 'parentNode', parentNode);
-
-      function nodeSetThunk(node) {
-        const setThunk = setThunkOfNodeType[node.type]; // all node types must be mapped
-        if (setThunk) {
-          //console.log(`setThunk for token ${node.type}`);
-          setThunk(node);
-        }
-        else if (setThunk === undefined) {
-          throw new NixEvalNotImplemented(`setThunk is empty for token ${node.type}`);
-        }
-      }
-
-      let currentNode = thisNode;
-      while (currentNode = currentNode.parent) {
-        for (const node of currentNode.children) {
-          nodeSetThunk(node);
-        }
-        //nodeSetThunk(currentNode); // no
-      }
-
-      //console.log('NixEval.evalTree: done convert. rootNode', rootNode); // FIXME this should be Nix
-
-      //console.log('NixEval.evalTree: final eval: rootNode.children[0]', rootNode.children[0]); // Nix
-
-      // TODO? continue walking nodes in tree
-
-      /*
-      // try to move up and right
-      if (cursor.parent() && cursor.nextSibling()) { // TODO verify
-        // moved up and right
-        parentNode = parentNode.parent;
-        continue;
-      }
-      */
-
-
-
-      // 3. Recursively traverse the current node's right subtree.
-      // case "right": if (this.right) this.right.walk(func, order); break;
-
-      //console.log(`step 3: thisNode:`); console.dir(thisNode);
-      //console.log(`${'  '.repeat(depth)}3. ${cursorType}: ${cursorText}`);
-
-      // move right and up
-
-      //console.log(`${'  '.repeat(depth)}4. ${cursorType}: ${cursorText}`);
-
-      while (true) {
-        if (cursor.nextSibling()) {
-          // moved right
-          break
-        }
-        if (cursor.parent()) {
-          // moved up
-          parentNode = parentNode.parent;
-          depth--;
-        }
-        else {
-          // not moved up
-          // done walking all nodes
-
-          if (rootNode.children[0].children.length == 0) {
-            // empty input
-            return undefined;
-          }
-
-          const evalResult = rootNode.children[0].thunk();
-          //console.log('NixEval.evalTree: evalResult', evalResult);
-          return evalResult;
-        }
-      }
-    }
+    return tree.topNode.type.thunk(tree.topNode, source);
   }
 }
-
-
