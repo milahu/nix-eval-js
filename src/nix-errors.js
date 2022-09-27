e75b0fec95ce7a87f9297376d8cd5ca3e2df29cf
Milan Hauth <milahu@gmail.com>
2022-09-27 17:58:59 +0200

    short error names

--- src/nix-errors.js
+++ src/nix-errors.js
@@ -1,20 +1,22 @@
 export class NixEvalError extends EvalError {
   constructor(message) {
     super(message);
-    this.name = this.constructor.name;
+    //this.name = this.constructor.name;
+    //this.name = "EvalError"
   }
 }
 
 export class NixEvalNotImplemented extends Error {
   constructor(message) {
     super(message);
-    this.name = this.constructor.name;
+    this.name = "EvalNotImplemented"
   }
 }
 
 export class NixSyntaxError extends SyntaxError {
   constructor(message) {
     super(message);
-    this.name = this.constructor.name;
+    //this.name = this.constructor.name;
+    //this.name = "SyntaxError"
   }
 }
